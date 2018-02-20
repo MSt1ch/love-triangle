eea218db1fd4d9128ea4da2918df9a4a4fbc54f2
St1ch <st1ch@mail.ru>
2018-02-21 02:05:32 +0300

    part solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
+ 	Spichonees (made-up name) live in country Spichland where are no gender. 
+ 	Spichonees are big lovers, so each Spichonee loves any another Spichonee. 
+ 	Unfortunately there are sometimes situations, when Spichonee A loves Spichonee B, 
+ 	Spichonee B loves Spichonee C and Spichonee C loves Spichonee A. 
+ 	This phenomenon is called Love triangle.
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  for (var i = 0; i <= preferences.length - 1; i++) {
+  	
+
+  	if(preferences[preferences[preferences[i] - 1] - 1] === i - 1){
+  		count += 1;
+  	}
+  }
+  return count;
 };
